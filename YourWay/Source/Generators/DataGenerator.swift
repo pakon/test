4974d9976715a0d9b91c44371680625b2a7455f6
Mike Rudoy <mike.rudoi@gmail.com>
2017-04-01 18:39:33 +0300

    Подправил данные для дело

--- YourWay/Source/Generators/DataGenerator.swift
+++ YourWay/Source/Generators/DataGenerator.swift
@@ -29,38 +29,48 @@ class DataGenerator {
         
         
         
-        let opportunity1 = Opportunity(title: "Шопинг 1",
+        let opportunity1 = Opportunity(title: "Прыжок с парашютом",
                                        backgroundURL: "https://s3.amazonaws.com/uploads.hipchat.com/82114/593081/ztu1PG7eKbDFs6t/Bitmap%403x.png",
-                                       desc: "Сегодня лучший день для покупок! Спешите на распродажу в ЦУМ и получите скидки на все покупки свыше 5 000 руб.", category: category1)
-        let opportunity2 = Opportunity(title: "Шопинг 2",
+                                       desc: "Посмотри своим страхам прямо в глаза! Сделай всего один шаг вперед, когда под ногами нет твердой опоры.", category: category2)
+        
+        let opportunity2 = Opportunity(title: "Красавица и чудовище",
                                        backgroundURL: "https://s3.amazonaws.com/uploads.hipchat.com/82114/593081/ztu1PG7eKbDFs6t/Bitmap%403x.png",
-                                       desc: "Сегодня лучший день для покупок! Спешите на распродажу в ЦУМ и получите скидки на все покупки свыше 5 000 руб.", category: category2)
-        let opportunity3 = Opportunity(title: "Шопинг 3",
-                                       backgroundURL: "https://s3.amazonaws.com/uploads.hipchat.com/82114/593081/ztu1PG7eKbDFs6t/Bitmap%403x.png",
-                                       desc: "Сегодня лучший день для покупок! Спешите на распродажу в ЦУМ и получите скидки на все покупки свыше 5 000 руб.", category: category3)
-        let opportunity4 = Opportunity(title: "Шопинг 4",
+                                       desc: "Обозлённая Волшебница превратила принца Адама в отвратительное чудовище за то, что тот был злым, самовлюблённым и грубым. Чтобы снять чары с себя и слуг своего замка, ему необходимо научиться быть добрым, любить и быть любимым. ", category: category3)
+        
+        let opportunity3 = Opportunity(title: "Время для шопинга",
                                        backgroundURL: "https://s3.amazonaws.com/uploads.hipchat.com/82114/593081/ztu1PG7eKbDFs6t/Bitmap%403x.png",
                                        desc: "Сегодня лучший день для покупок! Спешите на распродажу в ЦУМ и получите скидки на все покупки свыше 5 000 руб.", category: category4)
-        let opportunity5 = Opportunity(title: "Шопинг 5",
+        
+        let opportunity4 = Opportunity(title: "Ремонт в своей квартире",
                                        backgroundURL: "https://s3.amazonaws.com/uploads.hipchat.com/82114/593081/ztu1PG7eKbDFs6t/Bitmap%403x.png",
-                                       desc: "Сегодня лучший день для покупок! Спешите на распродажу в ЦУМ и получите скидки на все покупки свыше 5 000 руб.", category: category1)
-        let opportunity6 = Opportunity(title: "Шопинг 6",
+                                       desc: "Чтобы начать менять свою жизнь в лучшую сторону нужно сделать первый шаг. Отличная возможность начать с ремонта своей любимой комнаты.", category: category1)
+        
+        let opportunity5 = Opportunity(title: "Прыжок с парашютом",
                                        backgroundURL: "https://s3.amazonaws.com/uploads.hipchat.com/82114/593081/ztu1PG7eKbDFs6t/Bitmap%403x.png",
-                                       desc: "Сегодня лучший день для покупок! Спешите на распродажу в ЦУМ и получите скидки на все покупки свыше 5 000 руб.", category: category2)
-        let opportunity7 = Opportunity(title: "Шопинг 7",
+                                       desc: "Посмотри своим страхам прямо в глаза! Сделай всего один шаг вперед, когда под ногами нет твердой опоры.", category: category2)
+        
+        let opportunity6 = Opportunity(title: "Красавица и чудовище",
                                        backgroundURL: "https://s3.amazonaws.com/uploads.hipchat.com/82114/593081/ztu1PG7eKbDFs6t/Bitmap%403x.png",
-                                       desc: "Сегодня лучший день для покупок! Спешите на распродажу в ЦУМ и получите скидки на все покупки свыше 5 000 руб.", category: category3)
-        let opportunity8 = Opportunity(title: "Шопинг 8",
+                                       desc: "Обозлённая Волшебница превратила принца Адама в отвратительное чудовище за то, что тот был злым, самовлюблённым и грубым. Чтобы снять чары с себя и слуг своего замка, ему необходимо научиться быть добрым, любить и быть любимым. ", category: category3)
+        
+        let opportunity7 = Opportunity(title: "Время для шопинга",
                                        backgroundURL: "https://s3.amazonaws.com/uploads.hipchat.com/82114/593081/ztu1PG7eKbDFs6t/Bitmap%403x.png",
                                        desc: "Сегодня лучший день для покупок! Спешите на распродажу в ЦУМ и получите скидки на все покупки свыше 5 000 руб.", category: category4)
-        let opportunity9 = Opportunity(title: "Шопинг 9",
+        
+        let opportunity8 = Opportunity(title: "Ремонт в своей квартире",
                                        backgroundURL: "https://s3.amazonaws.com/uploads.hipchat.com/82114/593081/ztu1PG7eKbDFs6t/Bitmap%403x.png",
-                                       desc: "Сегодня лучший день для покупок! Спешите на распродажу в ЦУМ и получите скидки на все покупки свыше 5 000 руб.", category: category1)
+                                       desc: "Чтобы начать менять свою жизнь в лучшую сторону нужно сделать первый шаг. Отличная возможность начать с ремонта своей любимой комнаты.", category: category1)
+        
+        let opportunity9 = Opportunity(title: "Ремонт в своей квартире",
+                                       backgroundURL: "https://s3.amazonaws.com/uploads.hipchat.com/82114/593081/ztu1PG7eKbDFs6t/Bitmap%403x.png",
+                                       desc: "Чтобы начать менять свою жизнь в лучшую сторону нужно сделать первый шаг. Отличная возможность начать с ремонта своей любимой комнаты.", category: category1)
         
         
-        let step1 = Step(title: "Проведи день шопинга 1", order: 1, state: .missed)
-        let step2 = Step(title: "Проведи день шопинга 2", order: 2, state: .missed)
-        let step3 = Step(title: "Проведи день шопинга 3", order: 3, state: .missed)
+        
+        
+        let step1 = Step(title: "Новый вкус, ощущение или впечателение", order: 1, state: .missed)
+        let step2 = Step(title: "Новый вкус, ощущение или впечателение", order: 2, state: .missed)
+        let step3 = Step(title: "Новый вкус, ощущение или впечателение", order: 3, state: .missed)
         step1.opportunities.append(objectsIn: [opportunity1, opportunity2, opportunity3])
         step2.opportunities.append(objectsIn: [opportunity4, opportunity5, opportunity6])
         step3.opportunities.append(objectsIn: [opportunity7, opportunity8, opportunity9])
